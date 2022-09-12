f5fb10f4d47c61194f1011e8e1692cd368c979e4
masudsayyed <108341184+masudsayyed@users.noreply.github.com>
2022-09-12 14:06:50 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,12 @@
+ class Employee
+ { 
+ static String company = "Anudip";
+ static String manager = "Regional manager";
+ int id;
+ String name;
+ 
+  void employeeInfo()
+  {
+   System.out.println(" Company name: "+company+"\n Under: "+manager+"\n Employee id: "+id+"\n Employee name: "+name);
+  }
+ } 
