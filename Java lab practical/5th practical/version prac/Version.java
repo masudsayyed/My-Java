27b6b0fce382079f9c1c0e8cbd50b618add5444c
masudsayyed <108341184+masudsayyed@users.noreply.github.com>
2022-10-06 12:05:09 +0530

    Add files via upload

---
+++ Java lab practical/5th practical/version prac/Version.java
@@ -0,0 +1,17 @@
+/*
+ Question - Program find version of java
+ Name - Masud Sayyed.
+ Date - 06/10/2022
+ */
+ 
+package javaversion;
+
+public class Version {
+	
+	public static void main(String args[])
+	{
+		//to print version of java.
+		System.out.println("Java Version is: " + System.getProperty("java.version"));
+	}
+
+}
